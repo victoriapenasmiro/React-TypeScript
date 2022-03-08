a74874e35e9e4097ae2ad1044f4107503730d2c7
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2022-03-08 09:13:09 +0100

    first commit

---
+++ mi-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <h1>React + TypeScript</h1>
+      <hr />
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ mi-app/src/components/Counter.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const Counter = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const incrementar = () => {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div className="mt-5">
+            <h3>Counter: useState</h3>
+            <span>Valor: {counter} </span>
+            <br />
+            {/* idem === onClick={() => incrementar()} */}
+            <button onClick={incrementar} className="btn btn-outline-primary mt-2">+1</button>
+        </div>
+    )
+}
